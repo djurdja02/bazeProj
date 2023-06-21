babcd5325c9dec4c99de866b517d4e0c2f927597
Djurdja Joksimovic <jd200281d@student.etf.bg.ac.rs>
2023-06-21 22:02:16 +0200

    Izmenjen main, dodato dinamicko ucitavanje tacaka sa standardnog ulaza

--- projekatBaze/Main.java
+++ projekatBaze/Main.java
@@ -1,10 +1,17 @@
 package projekatBaze;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Rtree stablo=new Rtree(4, 2);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Unesite M i m u obliku M,m:");
+		String linija = scanner.nextLine();
+		int M=Integer.parseInt(linija.split(",")[0]);
+		int m=Integer.parseInt(linija.split(",")[1]);
+		Rtree stablo=new Rtree(M, m);
 		//Tacke za koje znamo redosled
 		/*for(int i=0;i<100;i++) {
 			double x=i;
@@ -24,10 +31,22 @@ public class Main {
 			*/
 		stablo.ucitajDatoteku("src/projekatBaze/"+args[0]);
 		stablo.ispis();
+		while(true) {
+			
+	        System.out.println("Unesite koordinate tacke u obliku X,Y:");
+	        linija = scanner.nextLine();
+	        if(linija.equals(""))break;
+	        double x=Double.parseDouble(linija.split(",")[0]);
+	        double y=Double.parseDouble(linija.split(",")[1]);
+	        stablo.dodaj(new Tacka(x,y));
+	        stablo.ispis();
+	        
+		}
+		scanner.close();
 		System.out.println("-------------------------------");
-		stablo.ispisiDubine();
+		//stablo.ispisiDubine();
 		System.out.println("-------------------------------");
-		stablo.pretrazivanje(new Region(new Tacka(0.1, 8.9),new Tacka(9.0,1.0)));
+		//stablo.pretrazivanje(new Region(new Tacka(6.1, 8.9),new Tacka(9.0,1.0)));
 		
 	}
 
